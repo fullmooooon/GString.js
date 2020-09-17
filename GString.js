433bf83978f18ed57d4837c97a890fa96ecee17f
wt <306859314@qq.com>
2020-09-17 16:36:31 +0800

    Update GString.js

--- GString.js
+++ GString.js
@@ -2,6 +2,6 @@
 module.exports.upper = ((source,index)=>{
   let frontPart=source.substring(0,index)
   let lastPart=source.substring(index+1)
-  let upperCharacter=source.substring(index,index+1)
+  let upperCharacter=source.substring(index,index+1).toUpperCase()
   return frontPart+upperCharacter+lastPart
 })
